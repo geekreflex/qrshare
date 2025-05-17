ebda0cc78ace81583e95b5c1a237431f914fb987
geekreflex <jerrynwosu007@gmail.com>
2025-05-17 22:34:01 +0100

    Setup tailwindcss

---
+++ web/src/components/FileExplorer.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+type FileItem = {
+  name: string;
+  isDir: boolean;
+  path: string;
+};
+
+export default function FileExplorer() {
+  const [files, setFiles] = useState<FileItem[]>([]);
+
+  useEffect(() => {
+    fetch("/api/files") // or just '/' depending on your Go backend
+      .then((res) => res.json())
+      .then((data) => setFiles(data))
+      .catch((err) => console.error("Error loading files:", err));
+  }, []);
+
+  return (
+    <div className="grid gap-3">
+      {files.map((file) => (
+        <div
+          key={file.path}
+          className="p-3 border rounded bg-white shadow flex justify-between items-center"
+        >
+          <span>{file.name}</span>
+          <a
+            href={`/files/${file.path}`}
+            className="text-blue-500 hover:underline"
+            download={!file.isDir}
+          >
+            {file.isDir ? "Open" : "Download"}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+}
